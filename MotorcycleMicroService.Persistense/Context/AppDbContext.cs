69158b707bcdf6f07673015e6508e79e0970b892
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-16 19:20:44 -0300

    feat: add persistence layer

---
+++ MotorcycleMicroService.Persistense/Context/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorcycleMicroService.Domain.Entities;
+using MotorcycleMicroService.Persistense.Mapping;
+
+namespace MotorcycleMicroService.Persistense.Context
+{
+    /// <summary>
+    /// Represents the application's database context, providing access to the database entities.
+    /// Inherits from <see cref="DbContext"/>.
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppDbContext"/> class with specified options.
+        /// </summary>
+        /// <param name="options">The options to be used by the DbContext.</param>
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// Gets or sets the <see cref="DbSet{TEntity}"/> for the <see cref="User"/> entity.
+        /// </summary>
+        public DbSet<Motorcycle> Motorcycles { get; set; }
+
+        /// <summary>
+        /// Configures the Entity Framework model when creating the entities in the database.
+        /// This method is called when the context is used for the first time.
+        /// </summary>
+        /// <param name="builder">A <see cref="ModelBuilder"/> used to build the model for the entities.</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new MotorcycleConfiguration());
+            //Add other configurations
+        }
+    }
+}
---
+++ MotorcycleMicroService.Persistense/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorcycleMicroService.Domain.Interfaces.Repositories;
+using MotorcycleMicroService.Persistense.Context;
+
+namespace MotorcycleMicroService.Persistense.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
