ada5340e9a5276b8f34d2685b363528dfc2a76eb
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-16 19:18:04 -0300

    remove: remove dtos in domain layer

--- MotorcycleMicroService.Domain/Dto/MotorcycleDto/Request/CreateMotorcycleRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MotorcycleMicroService.Domain.Dto.MotorcycleDto.Request
-{
-    public class CreateMotorcycleRequest
-    {
-        public string Name { get; set; }
-        public string YearManufacture { get; set; }
-        public string Manufacturer { get; set; }
-    }
-}
