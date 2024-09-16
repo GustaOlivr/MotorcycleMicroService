2b42f01b6a43495a907eb5beb8a6945c850b1696
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-16 19:18:30 -0300

    feat: add dtos in application layer

---
+++ MotorcycleMicroService.Application/Dto/MotorcycleDto/Request/CreateMotorcycleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MotorcycleMicroService.Application.Dto.MotorcycleDto.Request
+{
+    public class CreateMotorcycleRequest
+    {
+        public string Name { get; set; }
+        public string YearManufacture { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ MotorcycleMicroService.Application/Dto/MotorcycleDto/Response/UpdateMotorcycleResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MotorcycleMicroService.Application.Dto.MotorcycleDto.Response
+{
+    public class UpdateMotorcycleResponse
+    {
+        public Guid MotorcycleId { get; set; }
+        public string Name { get; set; }
+        public string YearManufacture { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
