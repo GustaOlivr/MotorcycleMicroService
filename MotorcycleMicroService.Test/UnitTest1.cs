93df86df1aa683c21a2ddceb9f41ea41545af299
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-23 22:18:10 -0300

    test: add unit test for CreateMotorcycleUseCase

---
+++ MotorcycleMicroService.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MotorcycleMicroService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
